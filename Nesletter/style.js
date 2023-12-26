bf8fe09626bf207dfa5c0c6aec43b8a0a885773b
Fohdil <fohdilulahiolayanju@gmail.com>
2023-12-26 12:43:39 +0000

    modified the html and css and added the Js code

---
+++ Nesletter/style.js
@@ -0,0 +1,31 @@
+function submitForm(event){
+
+    event.preventDefault();
+    var emailInput = document.getElementById('email');
+    var submittedEmail = document.getElementById('submittedEmail');
+    var successMessage = document.getElementById('message')
+    var subContainer = document.getElementById('sub-container')
+    var emailError = document.getElementById('emailError');
+
+    var emailRegex =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;;
+
+    if(!emailRegex.test(emailInput.value)) {
+        emailError.textContent = 'Valid email required'
+        emailInput.classList.add('error')
+    } else {
+        emailError.textContent = '';
+        emailInput.classList.remove('error');
+
+        submittedEmail.textContent = emailInput.value;
+    
+        successMessage.style.display = 'flex'; 
+        subContainer.style.display = 'none';
+
+        document.getElementById('emailForm').reset();
+    }
+}
+
+function showInfo(){
+    document.getElementById('message').style.display = 'none';
+    document.getElementById('sub-container').style.display = 'flex';
+}
