53a78c71cf702c92cfd38d403f9d4d6a7d3e6353
Fohdil <fohdilulahiolayanju@gmail.com>
2024-01-30 18:51:33 +0000

    added the functionality of the js and made some adjustment to the html and css file

--- faq/style.js
+++ faq/style.js
@@ -2,14 +2,38 @@ const opendiv = document.querySelectorAll('.questions');
 const showdetails = document.querySelectorAll('.answers')
 const changeicon = document.querySelectorAll('.icon-image')
 
-// console.log(changeicon)
-
 opendiv.forEach((element, index) => {
     element.addEventListener('click', () => {
-        showdetails.forEach(item => {
-            item.classList.remove('answer-visible')
-        })
-        showdetails[index].classList.add('answer-visible')
-        changeicon[index].src = 'images/icon-minus.svg'
+        toggleDetails(index)
     })
 })
+
+const openDetails = (index) => {
+    showdetails.forEach(item => {
+        item.classList.remove('answer-visible')
+    })
+
+    changeicon.forEach (icon => {
+        icon.src = 'images/icon-plus.svg'
+    })
+
+    showdetails[index].classList.add('answer-visible')
+}
+
+const closeDetails = (index) => {
+     showdetails[index].classList.remove('answer-visible')
+}
+
+const toggleDetails = (index) => {
+        if(showdetails[index].classList.contains('answer-visible')) {
+            closeDetails(index)
+            changeicon[index].src = 'images/icon-plus.svg'
+        }
+        else{
+            openDetails(index)
+            changeicon[index].src = 'images/icon-minus.svg'
+        }
+}
+
+
+
